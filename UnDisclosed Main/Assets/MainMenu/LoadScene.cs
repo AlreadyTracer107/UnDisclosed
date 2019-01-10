d713bf720b0f5684faa93815b92533f386b001ce
serocru01 <45695197+serocru01@users.noreply.github.com>
2019-01-10 12:28:14 -0500

    1/10
    Main Menu added and new colliders

---
+++ UnDisclosed Main/Assets/MainMenu/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
